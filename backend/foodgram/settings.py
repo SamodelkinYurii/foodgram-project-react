3b2a28601deb01f3f03d4ea60749b9026d945411
Юрий Самоделкин <samodelkinui@ya.ru>
2024-01-03 17:21:33 +0300

    добавлены tags

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -21,8 +21,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
     "api",
+    "colorfield",
+    "ingredients",
     "recipes",
+    "tags",
     "users",
 ]
 
---
+++ backend/tags/models.py
@@ -0,0 +1,24 @@
+from colorfield.fields import ColorField
+from django.db import models
+
+
+class Tag(models.Model):
+    """Модель тега."""
+
+    name = models.CharField(
+        "Наименование", help_text="Укажите наименование", max_length=200
+    )
+    color = ColorField("Цвет", help_text="Укажите цвет", max_length=7)
+    slug = models.SlugField(
+        "Слаг",
+        help_text="Укажите слаг, поле должно быть уникальным",
+        max_length=200,
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+
+    def __str__(self):
+        return self.name
