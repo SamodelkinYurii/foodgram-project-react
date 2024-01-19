7a272b1ef2f3232d14cd0de654930ba48c2d09dc
Юрий Самоделкин <samodelkinui@ya.ru>
2024-01-19 11:22:43 +0300

    foodgram v5.8

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -120,7 +120,7 @@ MEDIA_URL = "/media/"
 
 # MEDIA_ROOT = BASE_DIR / "media"
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = '/var/www/foodgram/media'
+MEDIA_ROOT = '/var/www/foodgram/media/'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
